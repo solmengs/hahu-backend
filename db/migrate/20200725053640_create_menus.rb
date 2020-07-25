2ce565a4cae105bc5b5391a422cdfd6171b12b2c
github username <github email address>
2020-07-24 23:01:38 -0700

    first commit

---
+++ db/migrate/20200725053640_create_menus.rb
@@ -0,0 +1,11 @@
+class CreateMenus < ActiveRecord::Migration[6.0]
+  def change
+    create_table :menus do |t|
+      t.string :name
+      t.integer :price
+      t.string :description
+
+      t.timestamps
+    end
+  end
+end
